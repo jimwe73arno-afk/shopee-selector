6db13e198a57bdc9df4ce53ef5a45248c61908c4
masterblue <jimwearno@brotherg.net>
2025-11-27 14:23:09 +0800

    config: 更新 authDomain 為 brotherg.ai

--- public/js/auth.js
+++ public/js/auth.js
@@ -4,7 +4,7 @@
 // Firebase 配置
 const firebaseConfig = {
   apiKey: "AIzaSyCbxeHkWKphUTUw4iQXuLvYgRsmjQOQYeg",
-  authDomain: "brothergai-699d2.firebaseapp.com",  // 先用 Firebase 預設，等 DNS 設定好再改
+  authDomain: "brotherg.ai",
   projectId: "brothergai-699d2",
   storageBucket: "brothergai-699d2.firebasestorage.app",
   messagingSenderId: "688590180498",
